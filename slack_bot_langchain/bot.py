642ae2e6db9fb9de6dab0f1e820c8f719ce00dd4
bharathkumartelu <bharathkumartelu@gmail.com>
2025-03-07 00:16:15 +0530

    working model

--- slack_bot_langchain/bot.py
+++ slack_bot_langchain/bot.py
@@ -1,35 +1,50 @@
 from slack_bolt import App
-from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import create_retrieval_chain
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+
 # Initialize
-app = App(
-    token=os.getenv("SLACK_BOT_TOKEN"),
-    signing_secret=os.getenv("SLACK_SIGNING_SECRET")
-)
+# app = App(
+#     token=os.getenv("SLACK_BOT_TOKEN"),
+#     signing_secret=os.getenv("SLACK_SIGNING_SECRET")
+# )
 
 # Load knowledge base
 vector_store = Chroma(
     persist_directory="./vector_db",
     embedding_function=OpenAIEmbeddings()
 )
-qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(temperature=0),
-    chain_type="stuff",
-    retriever=vector_store.as_retriever()
+
+retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+llm=ChatOpenAI()
+combine_docs_chain = create_stuff_documents_chain(
+    llm, retrieval_qa_chat_prompt
+)
+retriever=vector_store.as_retriever()
+
+print(retriever)
+
+qa = create_retrieval_chain(
+    retriever,
+    combine_docs_chain
 )
 
+print(qa.invoke({"input": "summary of software development space", }))
 # Handle messages
-@app.event("app_mention")
-def handle_mention(event, say):
-    response = qa.run(event["text"])
-    say(f"🤖: {response}")
+# @app.event("app_mention")
+# def handle_mention(event, say):
+# response = qa.run(event["text"])
+# say(f"🤖: {response}")
 
-if __name__ == "__main__":
-    app.start(port=3000)
+# if __name__ == "__main__":
+#     app.start(port=3000)
--- slack_bot_langchain/doc_loader.py
+++ slack_bot_langchain/doc_loader.py
@@ -1,28 +1,44 @@
-from langchain.document_loaders import GoogleDriveLoader, ConfluenceLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.document_loaders import GoogleDriveLoader
+from langchain_community.document_loaders import ConfluenceLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from uuid import uuid4
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-def build_knowledge_base():
+# def build_knowledge_base():
+print("...............................................")
     # Load Google Drive
-    gdrive_loader = GoogleDriveLoader(
-        folder_id=os.getenv("GOOGLE_DRIVE_FOLDER_ID"),
-        service_account_key=os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
-    )
-    drive_docs = gdrive_loader.load()
+    # gdrive_loader = GoogleDriveLoader(
+    #     folder_id=os.getenv("GOOGLE_DRIVE_FOLDER_ID"),
+    #     service_account_key=os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
+    # )
+    # drive_docs = gdrive_loader.load()
     
     # Load Confluence
-    confluence_loader = ConfluenceLoader(
-        url=os.getenv("CONFLUENCE_URL"),
-        username=os.getenv("CONFLUENCE_USER"),
-        api_key=os.getenv("CONFLUENCE_API_TOKEN")
-    )
-    confluence_docs = confluence_loader.load(space_key=os.getenv("CONFLUENCE_SPACE"))
+confluence_loader = ConfluenceLoader(
+    url=os.getenv("CONFLUENCE_URL"),
+    username=os.getenv("CONFLUENCE_USER"),
+    api_key=os.getenv("CONFLUENCE_API_TOKEN"),
+    space_key=os.getenv("CONFLUENCE_SPACE")
+)
+print(os.getenv("CONFLUENCE_URL"))
+
+confluence_docs = confluence_loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(confluence_docs)
+
     
     # Combine and vectorize
-    all_docs = drive_docs + confluence_docs
-    Chroma.from_documents(
-        documents=all_docs,
-        embedding=OpenAIEmbeddings(),
-        persist_directory="./vector_db"
-    )
+# all_docs = confluence_docs
+vector_store = Chroma(
+    embedding_function=OpenAIEmbeddings(),
+    persist_directory="./vector_db"
+)
+
+uuids = [str(uuid4()) for _ in range(len(all_splits))]
+
+vector_store.add_documents(documents=all_splits, ids=uuids)
