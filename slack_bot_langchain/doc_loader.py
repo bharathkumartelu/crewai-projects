570bcb4440724bcd677af620b56b8f142dc7006c
bharathkumartelu <147412012+bharathkumartelu@users.noreply.github.com>
2025-03-14 16:05:18 +0530

    Update doc_loader.py

--- slack_bot_langchain/doc_loader.py
+++ slack_bot_langchain/doc_loader.py
@@ -1,7 +1,8 @@
 from langchain_community.document_loaders import GoogleDriveLoader
 from langchain_community.document_loaders import ConfluenceLoader
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_chroma import Chroma
+# from langchain_google_community import GoogleDriveLoader
+from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from uuid import uuid4
 import os
@@ -11,34 +12,37 @@ load_dotenv()
 # def build_knowledge_base():
 print("...............................................")
     # Load Google Drive
-    # gdrive_loader = GoogleDriveLoader(
-    #     folder_id=os.getenv("GOOGLE_DRIVE_FOLDER_ID"),
-    #     service_account_key=os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
-    # )
-    # drive_docs = gdrive_loader.load()
+gdrive_loader = GoogleDriveLoader(
+    folder_id=os.getenv("GOOGLE_DRIVE_FOLDER_ID"),
+    service_account_key=os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE"),
+    file_types=("document", "sheet", "pdf", "presentation"),
+    recursive=True
+)
+print(os.getenv("GOOGLE_DRIVE_FOLDER_ID"))
+drive_docs = gdrive_loader.load()
     
-    # Load Confluence
+# Load Confluence
 confluence_loader = ConfluenceLoader(
     url=os.getenv("CONFLUENCE_URL"),
     username=os.getenv("CONFLUENCE_USER"),
     api_key=os.getenv("CONFLUENCE_API_TOKEN"),
-    space_key=os.getenv("CONFLUENCE_SPACE")
+    space_key=os.getenv("CONFLUENCE_SPACE"),
+    max_pages=2000
 )
-print(os.getenv("CONFLUENCE_URL"))
 
 confluence_docs = confluence_loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(confluence_docs)
+all_docs = drive_docs + confluence_docs
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
+all_splits = text_splitter.split_documents(all_docs)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     
     # Combine and vectorize
 # all_docs = confluence_docs
 vector_store = Chroma(
-    embedding_function=OpenAIEmbeddings(),
+    embedding_function=embeddings,
     persist_directory="./vector_db"
 )
 
-uuids = [str(uuid4()) for _ in range(len(all_splits))]
-
-vector_store.add_documents(documents=all_splits, ids=uuids)
+vector_store.add_documents(documents=all_splits)
